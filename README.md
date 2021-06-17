b78fc8611f2d14e80171c6beddbdfe6513c97010
Abigail Friedman <abigailfriedman@Abigails-MacBook-Air.local>
2021-06-17 12:48:58 -0400

    got through the event listener loop

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # m5-hw8-Friedman-Abigail
+
+https://www.w3schools.com/jquery/event_keyup.asp
+https://www.w3schools.com/js/js_loop_for.asp
--- index.js
+++ index.js
@@ -1,14 +1,51 @@
 // Create an Array of at least 3 losing messages
-
+var losingMessage = [
+"Sorry You lost",
+"You are not a winner",
+"You are still good at other things"
+];
 // Create variables to count wins and losses
-
+var winCount = 0
+var winLoss = 0
 // Create variables that target elements with the following IDs: 'message', 'wins', 'losses'
-
+var messageId = document.getElementById('message');
+var wins = document.getElementById('wins');
+var losses = document.getElementById('losses');
 // target all .box elements and attach a click event listener to each one using a loop
+//var boxesElement = document.querySelectorAll('box')
+
+//function boxClick(event) { 
+   // boxes.addEventListener("click", function()")
+//} 
+
+//for (i = 0; i < boxesElement.length; i++) {
+    //var box = boxesElement[i];
+    //box.onclick = boxClick
+//}
+
+//function boxClick () {
+   // console.log ("clicked");
+//}
+
+var boxesElement = document.querySelectorAll('.box')
+function boxClick(event) {
+    console.log(event.target)
+}
+for (i = 0; i < boxesElement.length; i++) {
+    var box = boxesElement[i];
+    box.onclick = boxClick
+}
+
 
 // within each click event...
 // determine which box was clicked with 'this.textContent' or event.target.textContent
-// convert that value to a Number and store it to a variable
+// convert that// value to a Number and store it to a variable
+
+boxesElement.forEach(function() {
+ box.onclick = function(e){
+
+ }
+});
 
 // create a random number between 1-3 and store it to a variable
 // This number will represent the winning box
